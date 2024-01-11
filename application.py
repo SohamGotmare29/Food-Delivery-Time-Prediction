3090de01343f44c5f009b67a8b50bbe700433a72
Soham Gotmare <142289702+SohamGotmare29@users.noreply.github.com>
2024-01-11 15:01:37 +0530

    deploment config for AWS

--- application.py
+++ application.py
@@ -2,7 +2,14 @@ from flask import Flask, render_template, request
 import pickle
 import numpy as np
 
+#logging
+import logging
+logging.basicConfig(filename="scrapper.log" , level=logging.DEBUG)
+
+#load model 
 model = pickle.load(open('newmodel.pkl', 'rb'))
+
+#Initializing a Flask API
 application = Flask(__name__)
 app=application
 
@@ -10,26 +17,26 @@ app=application
 def index():
     return render_template('indexs.html', result=None)  # Pass result as None initially
 
-@app.route('/predict', methods=['POST'])
+@app.route('/predict', methods=['POST','GET'])
 def predict_time():
-    Road_traffic_density = int(request.form.get('Road_traffic_density'))
-    multiple_deliveries = int(request.form.get('multiple_deliveries'))
-    Delivery_person_Ratings = int(request.form.get('Delivery_person_Ratings'))
-    Vehicle_condition = int(request.form.get('Vehicle_condition'))
-    Weather_conditions = int(request.form.get('Weather_conditions'))
-    Type_of_vehicle = int(request.form.get('Type_of_vehicle'))
-    Distance = float(request.form.get('Distance'))
-    Delivery_person_Age = int(request.form.get('Delivery_person_Age'))
+        Road_traffic_density = int(request.form.get('Road_traffic_density'))
+        multiple_deliveries = int(request.form.get('multiple_deliveries'))
+        Delivery_person_Ratings = int(request.form.get('Delivery_person_Ratings'))
+        Vehicle_condition = int(request.form.get('Vehicle_condition'))
+        Weather_conditions = int(request.form.get('Weather_conditions'))
+        Type_of_vehicle = int(request.form.get('Type_of_vehicle'))
+        Distance = float(request.form.get('Distance'))
+        Delivery_person_Age = int(request.form.get('Delivery_person_Age'))
 
-    features = [Road_traffic_density, multiple_deliveries, Delivery_person_Ratings,
-                Vehicle_condition, Weather_conditions, Type_of_vehicle, Distance, Delivery_person_Age]
+        features = [Road_traffic_density, multiple_deliveries, Delivery_person_Ratings,
+                    Vehicle_condition, Weather_conditions, Type_of_vehicle, Distance, Delivery_person_Age]
 
-    input_data = np.array(features).reshape(1, -1)
+        input_data = np.array(features).reshape(1, -1)
 
-    # prediction
-    result = model.predict(input_data)
+        # prediction
+        result = model.predict(input_data)
 
-    return render_template('indexs.html', result=result)
+        return render_template('indexs.html', result=result)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
